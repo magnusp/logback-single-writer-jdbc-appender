a1d8e55457c4baa747163073977ce2b9fd7c65b2
github-action-benchmark <github@users.noreply.github.com>
2026-09-11 18:07:28 +0000

    add JMH Benchmarks (SQLite Appender) (jmh) benchmark result for 1a070be1df84c6fb86a7ee89193fc8867bb6c62a

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1789150048974,
+  "repoUrl": "https://github.com/magnusp/logback-single-writer-jdbc-appender",
+  "entries": {
+    "JMH Benchmarks (SQLite Appender)": [
+      {
+        "commit": {
+          "author": {
+            "email": "1431685+magnusp@users.noreply.github.com",
+            "name": "Magnus Persson",
+            "username": "magnusp"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1a070be1df84c6fb86a7ee89193fc8867bb6c62a",
+          "message": "feat: implement resilient JDBC Logback appender with JMH benchmarks and tests (#2)",
+          "timestamp": "2026-09-11T20:06:48+02:00",
+          "tree_id": "9069f8c6560dde6253525d33fb1aac9968caf492",
+          "url": "https://github.com/magnusp/logback-single-writer-jdbc-appender/commit/1a070be1df84c6fb86a7ee89193fc8867bb6c62a"
+        },
+        "date": 1789150047998,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "com.github.magnusp.logback.resilientjdbc.ResilientJdbcAppenderBenchmark.benchmarkAppend",
+            "value": 46819998.66558317,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
